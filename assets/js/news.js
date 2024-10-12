6c22bde34c089fc9334a9f8116f3b9f26046c016
snehalchaudhari98 <1998snehalchaudhari@gmail.com>
2024-10-12 13:49:54 -0700

    collapsible news lists v3

---
+++ assets/js/news.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Toggle news content visibility
+    console.log("in newscript");
+    window.toggleNewsContent = function(element) {
+      const content = element.querySelector('.news-content');
+      console.log(content);
+      if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block"; // Show content
+      } else {
+        content.style.display = "none"; // Hide content
+      }
+    };
+
+    // Initialize all news content to be hidden on page load
+    document.querySelectorAll('.news-content').forEach(content => {
+      content.style.display = "none"; // Initially hide all content
+    });
+  });
